3a5f8921fff811a0bd9d0605742c415032a48d36
Brian Chu <brianchu26@berkeley.edu>
2024-08-05 14:18:15 -0700

    app layout

---
+++ firebase.js
@@ -0,0 +1,15 @@
+
+
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+const firebaseConfig = {
+    apiKey: "AIzaSyBjyYUB2ZOQwf5VrQa68gpotZ7vXNFqjPE",
+    authDomain: "inventory-6a3cc.firebaseapp.com",
+    projectId: "inventory-6a3cc",
+    storageBucket: "inventory-6a3cc.appspot.com",
+    messagingSenderId: "170335063983",
+    appId: "1:170335063983:web:7e78687c647ebc570e695a",
+};
+const app = initializeApp(firebaseConfig);
+const firestore = getFirestore(app);
+export { firestore };
